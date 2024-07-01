3ec97221421abcd2bece2ede0bf93c5cc90732a4
eventia <eventia@gmail.com>
2024-07-01 10:24:28 +0900

    gitTest Project 2024-07-01-[10:24]

--- a.bat
+++ a.bat
@@ -5,7 +5,7 @@ echo [PROCESSING...] git add --all .
 git add --all .
 if [%1] EQU [] (
 echo [PROCESSING...] git commit -m "gitTest Project %date%-[%time:~5,0%]"
-git commit -m "gitTest Project %date%-[%time:~5,0%]"
+git commit -m "gitTest Project %date%-[%time:~0,5%]"
 ) else (
 echo [PROCESSING...] git commit -m %1
 git commit -m %1
