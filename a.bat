0aa2b3354a795a3bfd5c84dfca234509b30a93b1
eventia <eventia@gmail.com>
2024-07-01 10:26:35 +0900

    AddRead2.txt

--- a.bat
+++ a.bat
@@ -4,7 +4,7 @@ git pull
 echo [PROCESSING...] git add --all .
 git add --all .
 if [%1] EQU [] (
-echo [PROCESSING...] git commit -m "gitTest Project %date%-[%time:~5,0%]"
+echo [PROCESSING...] git commit -m "gitTest Project %date%-[%time:~0,5%]"
 git commit -m "gitTest Project %date%-[%time:~0,5%]"
 ) else (
 echo [PROCESSING...] git commit -m %1
